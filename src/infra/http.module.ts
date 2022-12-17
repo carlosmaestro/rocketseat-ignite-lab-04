90f3fe27159334a61b837284a75560e91d706d7d
Carlos Maestro <chmmaestro@gmail.com>
2022-12-17 14:40:30 -0300

    aula 3/2 user cases, factories, mappers

--- src/infra/http.module.ts
+++ src/infra/http.module.ts
@@ -2,11 +2,23 @@ import { NotificationsController } from './http/controllers/notifications.contro
 import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from './database/database.module';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
   exports: [],
 })
 export class HttpModule {}
