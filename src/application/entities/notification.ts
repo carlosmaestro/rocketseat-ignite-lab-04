6432f56c5375a6b796eb304c8d16e98acd69d37e
Carlos Maestro <chmmaestro@gmail.com>
2022-12-17 00:33:35 -0300

    aula 3-1 mappers, view model, config paths

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,4 +1,4 @@
-import { Replace } from './../../helpers/Replace';
+import { Replace } from '@helpers/Replace';
 import { Content } from './content';
 import { randomUUID } from 'node:crypto';
 export interface NotificationProps {
@@ -6,6 +6,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -51,6 +52,14 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   public set recipientId(recipientId: string) {
--- src/infra/http.module.ts
+++ src/infra/http.module.ts
@@ -1,6 +1,6 @@
 import { NotificationsController } from './http/controllers/notifications.controller';
 import { Module } from '@nestjs/common';
-import { SendNotification } from 'src/application/use-cases/send-notification';
+import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
